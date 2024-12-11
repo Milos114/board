860383cfc4184f372f8dd081a9db721aded408c4
mstefanovic <milos.stefanovic@hemoterm.com>
2024-12-11 15:05:21 +0100

    fix

--- tests/Feature/Lane/LaneShowTest.php
+++ tests/Feature/Lane/LaneShowTest.php
@@ -4,10 +4,13 @@ namespace Tests\Feature\Lane;
 
 use App\Models\Lane;
 use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class LaneShowTest extends TestCase
 {
+    use RefreshDatabase;
+
     public function test_lane_can_be_shown(): void
     {
         $this->actingAs(User::factory()->create());
