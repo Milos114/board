e7faa5edce0b554a86186a97fa0e451e6bd745f1
mstefanovic <milos.stefanovic@hemoterm.com>
2025-01-30 15:43:08 +0100

    Enhance registration and login tests

--- tests/Feature/RegistrationTest.php
+++ tests/Feature/RegistrationTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -46,5 +47,46 @@ class RegistrationTest extends TestCase
             ->assertJsonStructure([
                 'access_token',
             ]);
+    }
+
+    public function test_registration_can_fail_with_invalid_data(): void
+    {
+        $response = $this->postJson('/api/register', [
+            'name' => $this->faker->name,
+            'email' => $this->faker->name,
+            'password' => 'password',
+            'c_password' => 'password',
+        ]);
+
+        $response->assertStatus(422)
+            ->assertJsonValidationErrors(['email']);
+    }
+
+    public function test_login_can_fail_with_invalid_data(): void
+    {
+        $user = User::create([
+            'name' => $this->faker->name,
+            'email' => $this->faker->unique()->safeEmail,
+            'password' => 'password',
+            'c_password' => 'password',
+        ]);
+
+        $response = $this->postJson('/api/login', [
+            'email' => $user->email,
+            'password' =>'wrong-password',
+        ]);
+
+        $response->assertStatus(401)
+            ->assertJson(['error' => 'Unauthorized']);
+    }
+
+    public function test_user_can_logout(): void
+    {
+        $token = auth()->login(User::factory()->create());
+
+        $response = $this->withHeader('Authorization', "Bearer $token")
+            ->postJson('/api/logout');
+
+        $response->assertStatus(200);
     }
 }
